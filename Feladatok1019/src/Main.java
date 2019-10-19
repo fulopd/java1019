5e7b488f7375533a1b424dad3ba6e16ad57a8f4b
Fülöp Dániel <fulopdaniel@gmail.com>
2019-10-19 10:14:33 +0200

    Változók MIN és MAX értékei

--- Feladatok1019/src/Main.java
+++ Feladatok1019/src/Main.java
@@ -2,7 +2,32 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// Első feladat:
+		// Készíts programot, mely megjeleníti a képernyőn a rendelkezésünkre
+		// álló számtípusok értelmezési tartományának minimumát és maximumát!
+		
+		System.out.println("Változók MIN és MAX értékei:");
+		System.out.println("Short Min: " + Short.MIN_VALUE);
+		System.out.println("Short Max: " + Short.MAX_VALUE);
+		
+		System.out.println("Byte Min: " + Byte.MIN_VALUE);
+		System.out.println("Byte Max: " + Byte.MAX_VALUE);
+		
+		System.out.println("Integer Min: " + Integer.MIN_VALUE);
+		System.out.println("Integer Max: " + Integer.MAX_VALUE);
+		
+		System.out.println("Float Min: " + Float.MIN_VALUE);
+		System.out.println("Float Max: " + Float.MAX_VALUE);
+		
+		System.out.println("Long Min: " + Long.MIN_VALUE);
+		System.out.println("Long Max: " + Long.MAX_VALUE);
+		
+		System.out.println("Double Min: " + Double.MIN_VALUE);
+		System.out.println("Double Max: " + Double.MAX_VALUE);
+		
+	
+		
 
 	}
 
